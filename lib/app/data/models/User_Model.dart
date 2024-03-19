e457e2c8287c8978f157470ca07aa749bb8fbe1b
Mostafa <nonatefa11@gmail.com>
2024-03-19 17:48:14 +0200

    add UserModel

--- lib/app/data/models/User_Model.dart
+++ lib/app/data/models/User_Model.dart
@@ -4,17 +4,23 @@ class UserModel {
   String? name;
   String? email;
   String? userid;
+  int? phoneNumber;
+  String? imageUrl;
 
   UserModel({
     required this.email,
     required this.userid,
     required this.name,
+    required this.imageUrl,
+    required this.phoneNumber,
   });
 
   UserModel.fromJcon({required Map<String, dynamic> data}) {
     name = data['name'];
     email = data['email'];
     userid = data['userid'];
+    imageUrl = data['imageUrl'];
+    phoneNumber = data['phoneNumber'];
   }
 
   Map<String, dynamic> toJcon() {
@@ -22,6 +28,8 @@ class UserModel {
       'name': name,
       'email': email,
       'userid': userid,
+      'imageUrl': imageUrl,
+      'phoneNumber': phoneNumber,
     };
   }
 }
