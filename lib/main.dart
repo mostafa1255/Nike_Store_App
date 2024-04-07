2808e11b117e026f4daf840fcb5a488263ebfb10
Mostafa <nonatefa11@gmail.com>
2024-04-07 19:13:29 +0200

    create bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nike_store_app/firebase_options.dart';
 import 'NikeStoreApp.dart';
+import 'app/core/Functions/simple_bloc_observer.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  Bloc.observer = SimpleBlocObserver();
   runApp(const NikeStoreApp());
 }
