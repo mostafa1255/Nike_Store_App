8ccd65a667f1066178c5a97dc9ca9bbc54e50261
Mostafa <nonatefa11@gmail.com>
2024-04-17 14:53:02 +0200

    add some functions to cart cubit

--- lib/app/data/manager/cart_Cubit/cart_cubit.dart
+++ lib/app/data/manager/cart_Cubit/cart_cubit.dart
@@ -1,30 +1,81 @@
+import 'dart:async';
 import 'package:bloc/bloc.dart';
+import 'package:flutter/foundation.dart';
 import 'package:meta/meta.dart';
+import 'package:nike_store_app/app/data/manager/home_cubits/OutDoor_cubit/out_door_cubit.dart';
 import 'package:nike_store_app/app/data/models/Products_Model.dart';
+import 'package:nike_store_app/app/data/repos/cart_repo/cart_repo.dart';
 import 'package:nike_store_app/app/data/repos/home_rep/home_repo.dart';
-
+import '../../models/cart_Model.dart';
 part 'cart_state.dart';
 
 class CartCubit extends Cubit<CartState> {
-  CartCubit({required this.homeRepo}) : super(CartInitial());
+  CartCubit({required this.cartRepo, required this.homeRepo})
+      : super(CartInitial());
   HomeRepo homeRepo;
+  CartRepo cartRepo;
+  Future<void> updateQuantityofProduct(
+      {required String id, required int quantity}) async {
+    if (quantity > 0) {
+      final result =
+          await cartRepo.updateQuantityofProduct(id: id, quantity: quantity);
+      result.fold(
+        (l) => emit(CartQuantityUpdatedFaliure(errMessage: l.errmessage)),
+        (r) => getCartProducts(),
+      );
+    }
+  }
 
   Future<void> addToCartProducts({required ProductsModel productsModel}) async {
     emit(AddtoCartProductsLoading());
-    final result = await homeRepo.addProductToCollection(
-        collectionName: "cart", productsModel: productsModel);
+    final result = await cartRepo.addProductWithQuantityToCart(
+        productsModel: productsModel);
     result.fold(
-      (l) => emit(AddtoCartProductsFailure(errMessage: l.errmessage)),
-      (r) => emit(AddtoCartProductsSuccsess()),
+      (l) {
+        print("Cart Products to failure");
+
+        emit(AddtoCartProductsFailure(errMessage: l.errmessage));
+      },
+      (r) {
+        print("Cart Products to succsess");
+
+        emit(AddtoCartProductsSuccsess());
+      },
     );
   }
 
   Future<void> getCartProducts() async {
-    final result =
-        await homeRepo.getProductsFromCollection(collectionName: "cart");
+    final result = await cartRepo.getAllProductswithQuantity();
     result.fold(
-      (l) => print("object"),
-      (r) => print("object"),
+      (l) {
+        emit(GetFromCartFailure(errMessage: l.errmessage));
+      },
+      (r) {
+        emit(GetFromCartSuccsesswithProducts(
+            products: r, totalPrice: getTotalPrice(r)));
+      },
+    );
+  }
+
+  double getTotalPrice(List<CartModel> products) {
+    double totalPrice = 0.0;
+    for (CartModel product in products) {
+      totalPrice += num.parse(product.productsModel.price!);
+    }
+    return totalPrice;
+  }
+
+  Future<void> deleteCartItem({required String productId}) async {
+    final result = await homeRepo.deleteProductFromCollection(
+      collectionName: "cart",
+      productId: productId,
+      subCollectionName: "quantity",
+    );
+    result.fold(
+      (l) {
+        emit(DeleteCartItemFailure(errMessage: l.errmessage));
+      },
+      (r) {},
     );
   }
 }
--- lib/app/data/manager/cart_Cubit/cart_state.dart
+++ lib/app/data/manager/cart_Cubit/cart_state.dart
@@ -14,3 +14,40 @@ final class AddtoCartProductsFailure extends CartState {
 
   AddtoCartProductsFailure({required this.errMessage});
 }
+
+final class GetFromCartLoading extends CartState {}
+
+final class GetFromCartSuccsesswithProducts extends CartState {
+  final List<CartModel> products;
+  final num totalPrice;
+  GetFromCartSuccsesswithProducts(
+      {required this.products, required this.totalPrice});
+}
+
+final class GetFromCartFailure extends CartState {
+  final String errMessage;
+
+  GetFromCartFailure({required this.errMessage});
+}
+
+final class GetFromCartSuccsesswithOutProducts extends CartState {}
+
+//final class DeleteCartItemSuccsess extends CartState {}
+
+final class DeleteCartItemFailure extends CartState {
+  final String errMessage;
+
+  DeleteCartItemFailure({required this.errMessage});
+}
+
+final class CartQuantityUpdatedSuccess extends CartState {
+  final int quantity;
+
+  CartQuantityUpdatedSuccess({required this.quantity});
+}
+
+final class CartQuantityUpdatedFaliure extends CartState {
+  final String errMessage;
+
+  CartQuantityUpdatedFaliure({required this.errMessage});
+}
