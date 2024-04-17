7a1aa1055a5f9da120ff4e91d34326244c35fd70
Mostafa <nonatefa11@gmail.com>
2024-04-17 14:50:13 +0200

    cache user info in sharedPrefrences

---
+++ lib/app/core/tools/save_user_info.dart
@@ -0,0 +1,49 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SaveUserInfo {
+  static Future<void> saveUserEmail(String email) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString("email", email);
+  }
+
+  // Get user email from shared preferences
+  static Future<String?> getUserEmail() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString("email");
+  }
+
+  // Save user phone to shared preferences
+  static Future<void> saveUserPhone(String phone) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString("phone", phone);
+  }
+
+  // Get user phone from shared preferences
+  static Future<String?> getUserPhone() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString("phone");
+  }
+
+  // Save user name to shared preferences
+  static Future<void> saveUserName(String name) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString("name", name);
+  }
+
+  // Get user name from shared preferences
+  static Future<String?> getUserName() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString("name");
+  }
+
+  static Future<void> saveUserUid(String uid) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString("uid", uid);
+  }
+
+  // Get user UID from shared preferences
+  static Future<String?> getUserUid() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString("uid");
+  }
+}
