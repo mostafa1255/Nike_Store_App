6ef777abb17d68651c771a3ebf42b890ed9bc451
Mostafa <nonatefa11@gmail.com>
2024-05-06 11:21:19 +0300

    create local data source

---
+++ lib/app/data/data_source/home_local_data_source/home_local_data_source.dart
@@ -0,0 +1,7 @@
+import 'package:nike_store_app/app/data/models/Products_Model.dart';
+
+abstract class HomeLocalDataSource {
+  List<ProductsModel> getAllProducts();
+  List<ProductsModel> getOutdoorProducts();
+  List<ProductsModel> getTennisProducts();
+}
---
+++ lib/app/data/data_source/home_local_data_source/home_local_data_source_impl.dart
@@ -0,0 +1,22 @@
+import 'package:nike_store_app/app/data/data_source/home_local_data_source/home_local_data_source.dart';
+import 'package:nike_store_app/app/data/models/Products_Model.dart';
+
+class HomeLocalDataSourceImpl extends HomeLocalDataSource {
+  @override
+  List<ProductsModel> getAllProducts() {
+    // TODO: implement getAllProducts
+    throw UnimplementedError();
+  }
+
+  @override
+  List<ProductsModel> getOutdoorProducts() {
+    // TODO: implement getOutdoorProducts
+    throw UnimplementedError();
+  }
+
+  @override
+  List<ProductsModel> getTennisProducts() {
+    // TODO: implement getTennisProducts
+    throw UnimplementedError();
+  }
+}
